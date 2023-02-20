76f1730695490b5b45590ff47080615569c06ace
“Muana” <lalhming.muana1982@gmail.com>
2023-02-20 18:15:01 +0530

    Create and Read endpoints completed

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
+
+// CRUD  create this part creates an user
 app.post('/users', (req, res)=>{
     const user = new User(req.body)
 
@@ -20,7 +22,38 @@ app.post('/users', (req, res)=>{
     
 })
 
+//CRUD read this part reads all users
+app.get('/users', (req, res)=>{
+    User.find({}).then((users)=>{
+        res.send(users);
+    }).catch((error)=>{
+        res.status(500).send()
+    })
+})
 
+
+// Read one user
+app.get('/users/:id',(req,res)=>{
+    
+    const _id = req.params.id
+
+    User.findById(_id).then((user)=>{
+
+        if(!user){
+            return res.status(500).send()
+        }
+
+        res.send(user)
+    }).catch((e)=>{
+        res.status(400).send()
+    })
+
+
+})
+
+
+
+// Create task
 app.post('/tasks', (req, res)=>{
     const task = new Task(req.body)
 
@@ -32,6 +65,32 @@ app.post('/tasks', (req, res)=>{
     
 })
 
+// Read all tasks
+app.get('/tasks',(req, res)=>{
+
+    Task.find({}).then((tasks)=>{
+        res.send(tasks);
+    }).catch((e)=>{
+        res.status(400).send()
+    })
+    
+})
+
+
+//Read one task
+app.get('/task/:id',(req, res)=>{
+    const _id= req.params.id
+
+    Task.findById((_id)).then((task)=>{
+        if(!task){
+            return req.status(500).send()
+        }
+
+        res.send(task);
+    }).catch((error)=>{
+        res.status(400).send();
+    })
+})
 
 
 

7ba5d6226bb3bca3cfb4b48e76f91a6508b00e10
Muana <lalhming.muana1982@gmail.com>
2023-02-20 11:31:57 +0530

    updated with new express code

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.post('/users', (req, res)=>{
+    res.send('Testing app')
+})
+
+app.listen(port,()=>{
+    console.log('Server is starting on port no:',port);
+})
+

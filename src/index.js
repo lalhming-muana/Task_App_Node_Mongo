f54416964c2ec10755d7b6cd4c3eb1d40b0ca9f9
“Muana” <lalhming.muana1982@gmail.com>
2023-02-21 16:16:52 +0530

    delete endpoints completed

--- src/index.js
+++ src/index.js
@@ -174,6 +174,39 @@ app.patch('/tasks/:id', async(req, res)=>{
 
 
 
+app.delete('/users/:id', async(req, res)=>{
+    try{
+        const user= await User.findByIdAndDelete(req.params.id)
+        
+        if(!user){
+            return res.status(404).send();
+        }
+
+        res.status(200).send(user)
+
+    } catch(e){
+        res.status(400).send(e);
+    }
+})
+
+
+app.delete('/tasks/:id', async(req, res)=>{
+
+    try{
+        const task = await Task.findByIdAndDelete(req.params.id);
+
+        if(!task){
+            res.status(400).send()
+        }
+        res.status(200).send(task)
+
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
+
+
+
 app.listen(port,()=>{
     console.log('Server is starting on port no:',port);
 })
